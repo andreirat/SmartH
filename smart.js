2dae2770a88ef7359cf7c27ebffe3a7b800f1413
andrei.rat <andreirat@ymail.com>
2017-06-11 14:27:51 +0300

    work

--- smart.js
+++ smart.js
@@ -4,9 +4,6 @@ var router = express.Router();
 var httpServer = require("http").createServer(app);
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
-
-var Oled = require('oled-js');
-var font = require('oled-font-5x7');
 var io = require('socket.io')(httpServer);
 var port = 3000;
 var board = new five.Board({
@@ -18,21 +15,35 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/pages/index.html');
 });
 httpServer.listen(port);
+console.log('Hello Rat ! Server is runing on port ' + port);
 
-console.log('Hello Rat ! Server is runing on port' + port);
+
+// Initialize
+var leds = {};
+
 
 // When board is ready ...
 board.on("ready", function() {
-
+    leds = new five.Leds(["P1-13", "P1-15"]);
 
 });
 
 io.on('connection', function(socket) {
     console.log('Started');
 
+
+    // Led ON action
     socket.on('led:on', function(data) {
-        var led = new five.Led("P1-13");
-        led.blink();
+        console.log(data.number);
+        leds[data.number].on();
+        console.log('Led ' + data.number + ' on');
+    });
+
+    // Led OFF action
+    socket.on('led:off', function(data) {
+        leds[data.number].off();
+        console.log('Led ' + data.number + ' off');
+
     });
 
 });
